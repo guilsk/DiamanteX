2b921bf989086036baeb75ecdf97c2fecc8ec231
guilsk <guilherme.gmdp@gmail.com>
2023-03-16 13:40:43 -0300

    Add project files.

---
+++ DiamanteX/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DiamanteX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.Write("Digite um número ímpar: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            while(x % 2 == 0)
+            {
+                Console.Write($"{x} não é impar! Digite outro número: ");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i < x; i+=2) 
+            {
+                for (int j = x; j > i; j-=2)
+                    Console.Write(" ");
+
+                for (int j = 0; j <= i; j++)
+                    Console.Write("x");
+
+                Console.WriteLine();
+            }
+
+            for (int i = x-2; i >= 0; i-=2)
+            {
+                for (int j = i; j < x+2; j+=2)
+                    Console.Write(" ");
+
+                for (int j = i; j > 0; j--)
+                    Console.Write("x");
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
